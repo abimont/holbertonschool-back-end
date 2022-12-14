fcc0fc67c7a76320251513cb12b920ae5fabd4d0
abimont <abimontes00@gmail.com>
2022-12-14 01:01:44 -0500

    Gather data from an API

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+from sys import argv
+import requests
+
+if __name__ == "__main__":
+    url = 'https://jsonplaceholder.typicode.com/users/' + argv[1]
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        content = response.json()
+        employee_name = content.get('name')
+        url = 'https://jsonplaceholder.typicode.com/users/' + \
+            argv[1] + '/todos'
+
+        response = requests.get(url)
+        if response.status_code == 200:
+            content = response.json()
+            number_done_tasks = 0
+            tasks_title = []
+
+            for dic in content:
+                if dic['completed'] is True:
+                    number_done_tasks += 1
+                    tasks_title.append(dic['title'])
+
+            print("Employee {} is done with tasks({}/{})".format(employee_name,
+                  number_done_tasks, len(content)))
+            [print("\t {}".format(title)) for title in tasks_title]
