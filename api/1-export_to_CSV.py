74b40585d2b60f6d40312b6b47ea2150b94f2765
abimont <abimontes00@gmail.com>
2022-12-16 23:00:02 -0500

    Export to CSV

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+extend your Python script to export data
+"""
+import requests
+from sys import argv
+
+
+def export_csv():
+    """ function that saves data into a csv file """
+    url = 'https://jsonplaceholder.typicode.com/users/' + argv[1]
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        content = response.json()
+        employee_name = content.get('name')
+        employee_id = argv[1]
+        url = 'https://jsonplaceholder.typicode.com/users/' + \
+            argv[1] + '/todos'
+    
+        response = requests.get(url)
+        if response.status_code == 200:
+            content = response.json()
+            filename = argv[1] + '.csv'
+
+            with open(filename, 'w') as file:
+                for dic in content:
+                    file.write('"{}", "{}", "{}", "{}"\n'.format(employee_id, employee_name, dic['completed'], dic['title']))
+
+            
+if __name__ == "__main__":
+    """ executing function """
+    export_csv()
